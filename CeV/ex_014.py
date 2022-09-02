9428d2105ef938444af0d6ca4c73f5b47d85ca8e
Felipe Barros <manofardk@gmail.com>
2022-09-02 16:31:52 +0044

    Exercicio 14

---
+++ CeV/ex_014.py
@@ -0,0 +1,6 @@
+print("="*20)
+print("Conversor de Celsius para Fafhenheit")
+print("="*20)
+num_user = float(input("Digite um valor em Celsius: "))
+fa = ((9 * num_user) / 5) + 32
+print(f"O valor de {num_user} Graus em Celsius equivale a {fa} Graus em Fafhenheit")
