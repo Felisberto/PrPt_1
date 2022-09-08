e75616e8109eb8bbaf2223ea84742b0127faec6a
Felipe Barros <manofardk@gmail.com>
2022-09-08 12:23:42 +0044

    Exercicio 7 media do aluno

---
+++ CeV/ex_007.py
@@ -0,0 +1,4 @@
+na_stu = str(input("Nome do aluno: "))
+not_stu_on = float(input("Nota P1: "))
+not_stu_se = float(input("Nota P2: "))
+print(f"O aluno {na_stu} ficou com {not_stu_on} na primeira prova e {not_stu_se} na segunda, tendo a média de {(not_stu_on+not_stu_se)/2} no total.")
