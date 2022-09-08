471da773038889d05744be48a3fe3c1c2d13f273
Felipe Barros <manofardk@gmail.com>
2022-09-08 12:27:00 +0044

    Exercicio 8 centimetros e milimetros

---
+++ CeV/ex_008.py
@@ -0,0 +1,2 @@
+met = float(input("Valor em metros: "))
+print(f"O valor digitado foi {met} metros. \nConvertido em centimetros fica {met*100} centimetros.\nConvertido em milimetros fica {met*1000} milimetros.")
