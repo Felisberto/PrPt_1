07ed5e91aef2a049c1f3fc6ecfcd6388d032f80d
Felipe Barros <manofardk@gmail.com>
2022-09-09 15:06:26 +0044

    Exercicio 13 aumento de 15%

---
+++ CeV/ex_013.py
@@ -0,0 +1,3 @@
+sala_fun = float(input("Salário do funcionário R$: "))
+n_sal = sala_fun + (sala_fun*15/100)
+print(f"O novo salário do funcionário foi de {sala_fun} reais para {n_sal} reais, com um aumento de 15%")
