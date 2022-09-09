4b6ba8247aa9d9aaa7802741bb01fc3bc8b1f1f0
Felipe Barros <manofardk@gmail.com>
2022-09-09 15:03:57 +0044

    Exercicio 11 largura e altura

---
+++ CeV/ex_011.py
@@ -0,0 +1,5 @@
+l_p = float(input("Largura: "))
+a_p = float(input("Altura: "))
+ar_p = l_p * a_p
+t_n = ar_p / 2
+print(f"A parede tem {ar_p} metros quadrados e precisa de {t_n} litros de tinta para pinta-la")
